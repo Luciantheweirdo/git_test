46919a406662752dd96588273362f5605567e389
Chen LU <chen.lu@stud.hs-hannover.de>
2025-11-24 14:44:21 +0100

    Summe ist imlementiert

--- Main.java
+++ Main.java
@@ -3,6 +3,12 @@ public class Main
 {
     public static void main(String args[])
     {
-        System.out.println("Hallo");
+        int summe = 0;
+        int og = 10;
+        
+        for (int i = 1; i <= og; ++i)
+        {
+            summe = summe + i;
+        }
     }
 }
