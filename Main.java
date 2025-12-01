feb2d3e3b9f6c70fb394f0c2e656286acd83157e
Chen LU <chen.lu@stud.hs-hannover.de>
2025-12-01 15:45:37 +0100

    Rebase Update 1

--- Main.java
+++ Main.java
@@ -21,7 +21,6 @@ public class Main
         }
         
         System.out.println("Summe = " + summe + " Mult = " + mult);
-        // Main Update 1
-        // Main Update 2
+        // Rebase Update 1
     }
 }
